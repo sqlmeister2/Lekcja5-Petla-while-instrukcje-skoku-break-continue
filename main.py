f7974eb39685c9949cffa2ce9b8d1a060134369c
Mariusz PC <sqlmeister2@gmail.com>
2023-11-04 21:06:14 +0100

    Oryginalny commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# zmienna inkrementacyjna
+i = 0
+
+#petla while
+while i < 5:
+    print(i)
+    i += 1
+
+#petla nieskonczona, ale z warunkiem i instrukcja break, która przerywa bieg pętli
+j = 0
+while True:
+    print(j)
+    j += 1
+    if j >= 5:
+        break
+
+k = 0
+while True:
+    k+= 1
+    if k % 2 == 1:
+        continue
+    print(k)
+    if k > 10:
+        break
+
+#z instrukcja exit, aby zakonczyc program
+# i = 0
+# while True:
+#     print(i)
+#     if(i == 5):
+#         print("hi")
+#         exit()
+#     i += 1
+
+#pętla while z else
+i = 1
+while i < 10:
+    print(i, "Hi")
+    i += 1
+else:
+    print("Koniec, Now i have ", i)
+
+
+
+
+
+
+
+
+
+
+
+
