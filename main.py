e3427cb28eee1d9ff0e731de096255251e03227b
MarioPC <sqlmeister2@gmail.com>
2024-10-23 22:27:10 +0200

    zakomentowanie kodów w celu uruchamiania kodów częsciami

--- main.py
+++ main.py
@@ -1,47 +1,52 @@
-# zmienna inkrementacyjna
-i = 0
+# # zmienna inkrementacyjna
+# i = 0
 
-#petla while
-while i < 5:
-    print(i)
-    i += 1
+# #petla while
+# while i < 5:
+#     print(i)
+#     i += 1
 
-#petla nieskonczona, ale z warunkiem i instrukcja break, która przerywa bieg pętli
-j = 0
-while True:
-    print(j)
-    j += 1
-    if j >= 5:
-        break
+# # #petla nieskonczona, ale z warunkiem i instrukcja break, która przerywa bieg pętli
+# j = 0
+# while True:
+#     print(j)
+#     j += 1
+#     if j >= 5:
+#         break
 
-k = 0
-while True:
-    k+= 1
-    if k % 2 == 1:
-        continue
-    print(k)
-    if k > 10:
-        break
 
-#z instrukcja exit, aby zakonczyc program
+##continue wraca do początku pętli
+# k = 0
+# while True:
+#     k+= 1
+#     if k % 2 == 1:
+#         continue
+#     print(k)
+#     if k > 10:
+#         break
+
+# #z instrukcja exit, aby zakonczyc program
 # i = 0
 # while True:
 #     print(i)
 #     if(i == 5):
-#         print("hi")
+#         print("do widzenia")
 #         exit()
 #     i += 1
 
-#pętla while z else
-i = 1
-while i < 10:
-    print(i, "Hi")
-    i += 1
-else:
-    print("Koniec, Now i have ", i)
+# #pętla while z else
+# i = 1
+# while i < 10:
+#     print(i, "Hi")
+#     i += 1
+# else:
+#     print("Koniec, Now i have ", i)
 
 
-
+# # Sprawdzanie poprawności działania funkcji exit()
+# print("Start")
+# exit() #jest jakis problem z wywołaniem tej funkcji w trybie interaktywnym
+# print("Koniec programu")
 
 
 
